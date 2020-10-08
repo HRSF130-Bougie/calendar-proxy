bb971362893cb5f6640f4e78b09299d7aa375e1f
Elana Cohen-Khani <elanadev@gmail.com>
2020-10-07 23:13:40 -0700

    Remove unnnecessary dependencies

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
-const axios = require('axios');
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 app.use(cors());
-app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
 //Harris - gallery
@@ -16,7 +13,7 @@ app.use('/listing/', createProxyMiddleware({ target: 'http://localhost:3001', ch
 app.use('/favorites/', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 app.use('/api/booking/listing/:id', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
 //app.use('/api/booking/listing/reservation', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
-app.use('/api/review/:id', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
+app.use('/api/reviews/:id', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
 app.use('/api/booking/listing/:id', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
 
 
