3a37bb05bdbad1d66f32b744e76e658a332c59ec
Elana Cohen-Khani <elanadev@gmail.com>
2020-10-30 23:58:04 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,14 +8,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
 
-//Harris - gallery
 app.use('/listing/', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 app.use('/favorites/', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 app.use('/api/booking/listing/:id', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
-//app.use('/api/booking/listing/reservation', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+app.use('/api/booking/listing/reservation', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
 app.use('/api/reviews/:id', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
 app.use('/api/booking/listing/:id', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
-
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
